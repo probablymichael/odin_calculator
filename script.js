f9f1669a96d7156da804e45156755f59b9ece1df
probablymichael <michqelz@protonmail.com>
2022-08-10 17:32:38 +0300

    Add functionality to delete btn

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* FUNCTIONS NEEDED */
 
-function resetData(){
+function resetDataAfterCalculation(){
     firstNumber = display.textContent;
     operation = '';
     secondNumber = '';
@@ -10,22 +10,22 @@ function resetData(){
 
 function add(a, b){
     display.textContent = +a + +b;
-    resetData()
+    resetDataAfterCalculation()
 }
 
 function subtract(a, b){
     display.textContent = +a - +b;
-    resetData()
+    resetDataAfterCalculation()
 }
 
 function multiply(a, b){
     display.textContent = +a * +b;
-    resetData()
+    resetDataAfterCalculation()
 }
 
 function divide(a, b){
     display.textContent = +a / +b;
-    resetData()
+    resetDataAfterCalculation()
 }
 
 /* DECLARATIONS */
@@ -34,6 +34,8 @@ const display = document.querySelector('.displayed')
 const num_btns = document.querySelectorAll('.num-btn')
 const operators = document.querySelectorAll('.operator')
 const equals = document.querySelector('.btn-equals')
+const clear = document.querySelector('.clear-btn')
+const backspace = document.querySelector('.backspace')
 
 /* STORE NUMBERS */
 let firstNumber = '';
@@ -58,25 +60,48 @@ num_btns.forEach(num_btns => {
 /* Add functionality to the operator buttons */
 operators.forEach(operators => {
     operators.addEventListener('click', (event) => {
-        if (operation == ''){
-            display.textContent += event.target.value
-            operation = event.target.value
-        }
+		if (operation == ''){
+			display.textContent += event.target.value
+        
+			operation = event.target.value
+		}
     })
 })
 
+/* OPERATE FUNCTION */
 function operate(operator, num1, num2){
     if (operator == '+'){
         add(num1, num2)
     } else if (operation == '-'){
         subtract(num1, num2)
-    } else if (operation == '×'){
+    } else if (operation == 'ร—'){
         multiply(num1, num2)
-    } else if (operation == '÷'){
+    } else if (operation == 'รท'){
         divide(num1, num2)
     }
 }
 
 equals.addEventListener('click', () => {
     operate(operation, firstNumber, secondNumber)
+})
+
+/* CLEAR BUTTON */
+clear.addEventListener('click', () => {
+    display.textContent = ''
+    firstNumber = '';
+    operation = '';
+    secondNumber = '';
+    praxis;
+    operatorIndex;
+})
+
+/* BACKSPACE */
+backspace.addEventListener('click', () => {
+    praxis = praxis.split('')
+    let praxisPop = praxis.pop()// praxis = praxis.slice(praxis.length - 1, praxis.length + 1)
+    if (operation !== ''){
+        operation = '';
+    }
+    praxis = praxis.join('')
+    display.textContent = praxis
 })
